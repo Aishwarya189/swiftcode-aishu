7ea9d7e78600ac51b9f4cd02fb03ce9c3213a9cb
Live session user <aishwarya.seshachala@gmail.com>
2017-09-16 07:52:25 +0000

    improved commit

--- public/javascript/chat.js
+++ public/javascript/chat.js
@@ -1,4 +1,24 @@
 var app=angular.module('chatApp',['ngMaterial']);
 app.controller('chatController',function($scope){
+	$scope.messages = [{
+		sender:"BOT" ,
+		text:"hellooo",
+		time:"1:29PM"
+	},
+	{
+		sender:"USER",
+		text:"heyy",
+		time:"1:30PM"
+	},
+	{
+		sender:"USER",
+		text:"good afternoon",
+		time:"1:30PM"
+	}];
+var exampleSocket = new  WebSocket("ws://localhost:9000/chatSocket");
+exampleSocket.onmessage  =   function  (event) {
+       var jsonData = JSON.parse(event.data);
+       console.log(jsonData);
+   };
 
 });
