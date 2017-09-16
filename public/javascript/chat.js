f5ce940fb609405e0ee30615b9c7dae78939d12a
Live session user <aishwarya.seshachala@gmail.com>
2017-09-16 10:33:44 +0000

    THIRD DAY COMMIT

--- public/javascript/chat.js
+++ public/javascript/chat.js
@@ -1,24 +1,22 @@
 var app=angular.module('chatApp',['ngMaterial']);
-app.controller('chatController',function($scope){
-	$scope.messages = [{
-		sender:"BOT" ,
-		text:"hellooo",
-		time:"1:29PM"
-	},
-	{
-		sender:"USER",
-		text:"heyy",
-		time:"1:30PM"
-	},
-	{
-		sender:"USER",
-		text:"good afternoon",
-		time:"1:30PM"
-	}];
+app.controller('chatController',function($scope, $sce){
+	$scope.messages = [];
+	$scope.trust = $sce.trustAsHtml;
+	$scope.sendMessage = function () {    
+        exampleSocket.send($scope.userMessage);
+
+        $scope.userMessage = "";
+};
+
 var exampleSocket = new  WebSocket("ws://localhost:9000/chatSocket");
 exampleSocket.onmessage  =   function  (event) {
+       
        var jsonData = JSON.parse(event.data);
+        jsonData.time = new Date().toLocaleTimeString();
+        $scope.messages.push(jsonData);
+        $scope.$apply(); 
        console.log(jsonData);
    };
+
 
 });
